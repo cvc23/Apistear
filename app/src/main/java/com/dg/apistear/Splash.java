2dcb2743990debd2e9c1afb97c534b336f83418f
Kevin Erick <kevin.angel@tecmor.mx>
2017-06-30 00:27:09 -0500

    splash

--- app/src/main/java/com/dg/apistear/Splash.java
+++ app/src/main/java/com/dg/apistear/Splash.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.felipecsl.gifimageview.library.GifImageView;
+import org.apache.commons.io.IOUtils;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Splash extends AppCompatActivity {
+
+    private GifImageView gifImageView;
 
     private final int SPLASH_DISPLAY_LENGTH = 2000;
 
@@ -14,6 +20,15 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        gifImageView = (GifImageView)findViewById(R.id.gifImageView);
+
+        try{
+            InputStream inputStream = getAssets().open("sgdg.gif");
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+            gifImageView.setBytes(bytes);
+            gifImageView.startAnimation();
+        }
+        catch (IOException ex) {}
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
