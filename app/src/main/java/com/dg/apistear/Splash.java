7522c9776a3935652f8232441317e9da8995d647
Kevin Erick <kevin.angel@tecmor.mx>
2017-06-30 23:38:24 -0500

    splash

--- app/src/main/java/com/dg/apistear/Splash.java
+++ app/src/main/java/com/dg/apistear/Splash.java
@@ -1,34 +1,25 @@
 package com.dg.apistear;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.felipecsl.gifimageview.library.GifImageView;
-import org.apache.commons.io.IOUtils;
-import java.io.IOException;
-import java.io.InputStream;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
 
-    private GifImageView gifImageView;
-
-    private final int SPLASH_DISPLAY_LENGTH = 2000;
+    private final int SPLASH_DISPLAY_LENGTH = 3100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        gifImageView = (GifImageView)findViewById(R.id.gifImageView);
-
-        try{
-            InputStream inputStream = getAssets().open("sgdg.gif");
-            byte[] bytes = IOUtils.toByteArray(inputStream);
-            gifImageView.setBytes(bytes);
-            gifImageView.startAnimation();
-        }
-        catch (IOException ex) {}
+        ImageView GifSplash= (ImageView) findViewById(R.id.imageViewSplash);
+        GifSplash.setImageResource(R.drawable.gifsplash);
+        AnimationDrawable GifBeer = (AnimationDrawable) GifSplash.getDrawable();
+        GifBeer.start();
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
